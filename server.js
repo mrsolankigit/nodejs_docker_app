90622762d1c1b44f171581dd1a1a6918ae46c49b
mrsolankigit <71563278+mrsolankigit@users.noreply.github.com>
2025-06-20 15:03:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.get("/", (req, res) => {
